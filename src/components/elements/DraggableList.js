9b3887cf0ee34bb4b430e8d6c14374604239feef
arcolog <arcolog@gmail.com>
2021-01-26 19:04:39 +0200

    implement draggable list

--- src/components/elements/DraggableList.js
+++ src/components/elements/DraggableList.js
@@ -1,31 +1,99 @@
-import React from 'react';
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import PropTypes from 'prop-types';
-import { Grid } from '@material-ui/core';
+import DragIcon from '@material-ui/icons/DragIndicator';
+import { LIST_TYPES } from '../../constants';
 
-const DraggableList = ({ title, description, items, labelIsEditable = true }) => {
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+};
+
+const DraggableList = ({
+  items = [],
+  ItemComponent,
+  type,
+  handleDragEndAsync = () => {},
+  isVertical = true,
+  width = 250,
+}) => {
+
+  const grid = 8;
+  const [sortedItems, setSortedItems] = useState(items);
+
+  const getItemStyle = (isDragging, draggableStyle) => ({
+    userSelect: 'none',
+    padding: grid * 2,
+    paddingLeft: 36, // for drag icon
+    position: 'relative',
+    margin: `0 0 ${grid}px 0`,
+    background: isDragging ? 'lightblue' : '#f0f0f0',
+    ...draggableStyle
+  });
+
+  const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? '#ffa' : '#fafafa',
+    padding: grid,
+    display: 'flex',
+    flexDirection: isVertical ? 'column' : 'row',
+    width,
+  });
+
+  const onDragEnd = async result => {
+    if (!result.destination || result.source.index === result.destination.index) {
+      return;
+    }
+    const reSortedItems = reorder(sortedItems, result.source.index, result.destination.index)
+      .map((item, key) => ({ ...item, priority: key }));
+    setSortedItems(reSortedItems);
+    await handleDragEndAsync(type, reSortedItems);
+  }
+
   return (
-    <div style={{ maxWidth: 500 }}>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      {items.map(item => (
-        <Grid key={item.id} container justify="space-between" alignItems="center">
-          <Grid item>{item.label}</Grid>
-          <Grid item>{item.url}</Grid>
-        </Grid>
-      ))}
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="droppable">
+        {(provided, snapshot) => (
+          <div
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            style={getListStyle(snapshot.isDraggingOver)}
+          >
+            {sortedItems.map((item, index) => (
+              <Draggable key={item.label} draggableId={item.label} index={index}>
+                {(provided, snapshot) => (
+                  <div ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+                  >
+                    <DragIcon size="large" style={{ position: 'absolute', left: 5, top: 15 }} />
+                    <ItemComponent item={item} />
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
 
 DraggableList.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
   items: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number,
     label: PropTypes.string,
     url: PropTypes.string,
     priority: PropTypes.number,
   })),
+  ItemComponent: PropTypes.node.isRequired,
+  type: PropTypes.oneOf(Object.keys(LIST_TYPES)).isRequired,
+  handleDragEndAsync: PropTypes.func, // (type, items) as props
+  isVertical: PropTypes.bool,
+  width: PropTypes.string, // hint: set it '100%' for horizontal list
 }
 
 export default DraggableList;
--- src/pages/LinksPage.js
+++ src/pages/LinksPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import { Grid } from '@material-ui/core';
 
-import { LIST_TYPE_SOCIAL, SOCIAL_MEDIA_TYPES } from '../constants';
+import { LIST_TYPES, LIST_TYPE_SOCIAL, SOCIAL_MEDIA_TYPES } from '../constants';
 import { fetchMenuListAsync } from '../api';
 import DraggableList from '../components/elements/DraggableList';
 import LoadingBackdrop from '../components/elements/LoadingBackdrop';
@@ -25,9 +26,23 @@ const LinksPage = ({
     return <LoadingBackdrop />
   }
 
+  const handleDragEndAsync = (type, items) => {
+    setSocialItems(items);
+    console.log('handle type', type, items);
+  }
+
+  const ItemComponent = ({ item }) => (
+    <Grid container justify="space-between" alignItems="center">
+      <Grid item>{item.label}</Grid>
+      <Grid item>{item.url}</Grid>
+    </Grid>
+  );
+
   return (
     <div>
-      <DraggableList items={socialItems} {...SOCIAL_MEDIA_TYPES[LIST_TYPE_SOCIAL]} />
+      <h2>{LIST_TYPES[LIST_TYPE_SOCIAL].title}</h2>
+      <p>{LIST_TYPES[LIST_TYPE_SOCIAL].description}</p>
+      <DraggableList {...{ items: socialItems, ItemComponent, handleDragEndAsync, type: LIST_TYPE_SOCIAL }} />
     </div>
   )
 }
