60e5a9e8754d607ba0afaac2e8f12c4bb52e5e28
arcolog <arcolog@gmail.com>
2021-02-06 19:29:29 +0200

    remove buggy MUI Backdrop

--- src/components/elements/LoadingBackdrop.js
+++ src/components/elements/LoadingBackdrop.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { Backdrop, CircularProgress } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { CircularProgress } from '@material-ui/core';
+
+const backdropStyle = {
+	zIndex: 100,
+	color: '#fff',
+	width: '100vw',
+	height: 'calc(100vh - 64px)',
+	background: 'rgba(100, 140, 180, 0.5)',
+	position: 'fixed',
+	left: 0,
+	top: '64px',
+	textAlign: 'center',
+};
 
 const LoadingBackdrop = ({
-	open= true,
-	handleClose = () => {},
 	color = 'primary',
-	size = 40,
-	thickness = 4,
+	size = 100,
+	thickness = 2,
 }) => (
-	<Backdrop open={open} onClick={handleClose}>
-		<CircularProgress disableShrink {...{ color, size, thickness }} />
-	</Backdrop>
+	<div style={backdropStyle}>
+		<CircularProgress disableShrink {...{ color, size, thickness }} style={{ marginTop: 100 }} />
+	</div>
 );
+
+LoadingBackdrop.propTypes = {
+	color: PropTypes.oneOf(['primary', 'secondary', 'inherit']),
+	size: PropTypes.number,
+	thickness: PropTypes.number,
+}
 
 export default LoadingBackdrop;
